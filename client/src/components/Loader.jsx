1ac5074a76c4f469f8dfc218dbfa53996ad70fa1
Moisés Alvarenga <oscar-alvarenga35@fpuna.edu.py>
2023-04-12 11:31:30 -0400

    Loader component add

---
+++ client/src/components/Loader.jsx
@@ -0,0 +1,20 @@
+import './Loader.css'
+
+const Loader = () => {
+    return (
+        <div className="loadingio-spinner-double-ring-dvw283io08l">
+            <div className="ldio-knikc95u3c">
+                <div></div>
+                <div></div>
+                <div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
